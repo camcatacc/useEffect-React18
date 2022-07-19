124f31d1261c2b90874ccb63ae8f2ce7b5ae2ea4
David Camacho Cateura <david.camacho@accenture.com>
2022-07-19 15:46:14 +0200

    Implemented a fetching image service

---
+++ src/services/images.ts
@@ -0,0 +1,14 @@
+import { v4 as uuidv4 } from "uuid";
+
+export const getImages = async (num: number): Promise<string[]> => {
+	const images = [...Array(num)].map(() => `https://picsum.photos/200/300?random=${uuidv4()}`);
+	return fetch("https://jsonplaceholder.typicode.com/posts", {
+		method: "POST",
+		body: JSON.stringify({ images }),
+		headers: {
+			"Content-type": "application/json; charset=UTF-8"
+		}
+	})
+		.then((response) => response.json())
+		.then((response) => response.images);
+};
