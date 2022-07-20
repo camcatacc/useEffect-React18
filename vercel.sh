27485cf261d2b6c1349c16e67ea32d66e4cad12a
David Camacho Cateura <david.camacho@accenture.com>
2022-07-20 11:02:54 +0200

    tried to fix development env

--- vercel.sh
+++
@@ -1,7 +0,0 @@
-if [[ $VERCEL_GIT_COMMIT_REF == "master"  ]] ; then 
-  echo "This is our master branch"
-  npm run build:prod
-else 
-  echo "This is not our main branch"
-  NODE_ENV=development npm run build:dev
-fi
