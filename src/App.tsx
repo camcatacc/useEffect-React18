c12cc65eb7c78dc44a41ada75ff3b2aaccf1c736
David Camacho Cateura <david.camacho@accenture.com>
2022-07-20 09:25:57 +0200

    Enhanced App.tsx with some links and explanations

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,16 @@
-import { Link } from "react-router-dom";
+import { Link, Outlet } from "react-router-dom";
 import "./App.css";
 
 function App() {
 	return (
 		<div className="App">
-			<Link to="console" />
-			<Link to="regular-image-fetching" />
-			<Link to="react-query-image-fetching" />
+			<h1>useEffect Demo</h1>
+			<nav className="linksContainer">
+				<Link to="/">Console</Link>
+				<Link to="regular-image-fetching">Regular image fetching</Link>
+				<Link to="react-query-image-fetching">React Query Image Fetching</Link>
+			</nav>
+			<Outlet />
 		</div>
 	);
 }
