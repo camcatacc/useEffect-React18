503af7b787b14566aa86f79ffda426e7285626cc
David Camacho Cateura <david.camacho@accenture.com>
2022-07-20 09:27:30 +0200

    Added a debugger when the service is called

--- src/services/images.ts
+++ src/services/images.ts
@@ -1,6 +1,7 @@
 import { v4 as uuidv4 } from "uuid";
 
 export const getImages = async (num: number): Promise<string[]> => {
+	debugger;
 	const images = [...Array(num)].map(() => `https://picsum.photos/200/300?random=${uuidv4()}`);
 	return fetch("https://jsonplaceholder.typicode.com/posts", {
 		method: "POST",
